4106976c4fe5829a694486c41b282ca79187d6c3
Matthew Morrone <matthewmorrone1@gmail.com>
2014-12-17 19:53:21 -0500

    not totally sure what's changed

---
+++ pivot.js/Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem "rake"
+gem "uglifier"
+gem 'faker'
+gem 'pry'
+gem 'ruby_gntp'
+gem 'guard-shell'
+gem "jsduck"
--- server.php
+++ server.php
@@ -1,30 +1,44 @@
 <?
-if ($_POST)
+if (extract($_POST) > 0 || extract($_GET) > 0)
 {
-	extract($_POST);
+	// if ($output == "xml")
+	// {
+	// 	// header('Content-Type: text/xml');
+	// }
+
 
 	$server 	= (isset($server) 		? $server 		: "localhost");
 	$username	= (isset($username) 	? $username 	: "root");
 	$password 	= (isset($password) 	? $password 	: "root");
-	// $database 	= (isset($database) 	? $database 	: "");
+	$database 	= (isset($database) 	? $database 	: "");
+	// $output 	= (isset($output) 		? $output 		: "json");
 
-	$link = connect($account, $username, $password, $database) or die(mysqli_error($link));
+
+	$link = connect($server, $username, $password, $database) or die(mysqli_error($link));
 
 
 	switch($mode)
 	{
 		case "show-databases":
-			echo json_query($link, "SHOW DATABASES;");
+			echo prepare(show_databases($link));
+			// http://localhost/spreadsheet/server.php?server=localhost&username=root&password=root&mode=show-databases
 		break;
 		case "show-tables":
-			echo json_query($link, "SHOW TABLES FROM $database;");
+			echo prepare(show_tables($link, $database));
+			// http://localhost/spreadsheet/server.php?server=localhost&username=root&password=root&database=assig3&mode=show-tables
 		break;
-		case "show-columns":
-			echo json_encode(pluck(get_query($link, "SHOW FIELDS FROM $table;")));
+		case "show-fields":
+			echo prepare(show_fields($link, $table));
+			// http://localhost/spreadsheet/server.php?server=localhost&username=root&password=root&database=assig3&mode=show-fields&table=players
 		break;
-		case "select-star":
-			echo json_query($link, "SELECT * FROM $table;");
+		case "select":
+			echo prepare(select_from($link, $table, $fields, $include_fields, $entrylimit, $entryfrom, $entryto));
+			// http://localhost/spreadsheet/server.php?server=localhost&username=root&password=root&database=assig3&mode=select&table=players&fields=*
+			// http://localhost/spreadsheet/server.php?server=localhost&username=root&password=root&database=assig3&mode=select&table=players&fields=*&include_fields=true
 		break;
+		// case "debug":
+		// 	print_r($_POST);
+		// break;
 	}
 
 
@@ -38,51 +52,106 @@ function connect($account, $username, $password, $database)
 	endif;
 	return $link;
 }
-function pluck($array, $i = 0)
+function prepare($result, $fields)
 {
-	$array2 = [];
-	foreach($array as $key=>$val)
-	{
-		$array2[] = $val[$i];
-	}
-	return $array2;
+	return strToLower(json_encode($result));
 }
-function json_query($link, $query) 
+function show_databases($link)
 {
-	return json_encode(get_query($link, $query));
+	$result = get_query($link, "SHOW DATABASES;");
+	foreach($result as $key=>$row):
+		$databases[] = $row["Database"];
+	endforeach;
+	return $databases;
+}
+function show_tables($link, $database)
+{
+	$result = get_query($link, "SHOW TABLES FROM $database;");
+	foreach($result as $key=>$row):
+
+		$tables[] = $row["Tables_in_".$database];
+	endforeach;
+	return $tables;
+}
+function show_fields($link, $table)
+{
+	$result = get_query($link, "SHOW FIELDS FROM $table;");
+	foreach($result as $key=>$row):
+		$fields[] = $row["Field"];
+	endforeach;
+	// echo "<pre>"; print_r($fields); echo "</pre>";
+	return $fields;
+
+}
+function select_from($link, $table, $fields, $show_fields, $limit=30, $from=0, $to=30)
+{
+	$query = "SELECT $fields FROM $table";
+	$result = $link->query(trim($query));
+
+
+
+	echo $result->num_rows;
+	echo "###";
+
+
+
+
+
+	$query = "SELECT $fields FROM $table";
+	$query .= " WHERE id >= $from AND id <= $to";
+	$query .= " LIMIT $limit";
+	$query .= ";";
+
+
+
+	$result = $link->query(trim($query));
+
+
+
+
+
+
+	if ($show_fields == "yes")
+	{
+		if ($fields === "*"):
+			$fields = show_fields($link, $table);
+		else:
+			$fields = explode(",", $fields);
+		endif;
+		$res[] = array_intersect($fields, show_fields($link, $table));
+	}
+	for($i = 0; $i < $result->num_rows; $i++):
+		$res[] = $result->fetch_row();
+	endfor;
+	foreach($res as $key=>$row):
+		$rows[] = $row;
+	endforeach;
+	return $rows;
 }
 function get_query($link, $query)
 {
-	trim($query);
-	$result = $link->query($query);
-	$num_rows = $result->num_rows;
-	for($i = 0; $i < $num_rows; $i++):
-		$row = $result->fetch_row(); //try fetch_array and see what happens
-		for($j = 0; $j < count($row); $j++):
-			$res[$i][$j] = $row[$j];
-		endfor;
+	$result = $link->query(trim($query));
+	for($i = 0; $i < $result->num_rows; $i++):
+		$res[] = $result->fetch_assoc();
 	endfor;
 	return $res;
 }
 function print_query($link, $query)
 {
-	trim($query);
-	$result = $link->query($query);
-	$num_rows = $result->num_rows;
-	$fields = $result->fetch_fields();
+	$result = $link->query(trim($query));
 	echo "<table>";
-	for($i = 0; $i < $num_rows; $i++):
+	for($i = 0; $i < $result->num_rows; $i++):
 		$row = $result->fetch_assoc();
 		if ($i == 0):
 			echo "<tr>";
 			foreach($row as $field=>$cell):
-				echo "<th>".$field."</th>";
+				echo "<th>$field</th>";
 			endforeach;
 			echo "</tr>";
 		endif;
 		echo "<tr>";
 		foreach($row as $field=>$cell):
-			echo "<td>".$cell."</td>";
+			echo "<td>$cell</td>";
 		endforeach;
 		echo "</tr>";
 	endfor;
